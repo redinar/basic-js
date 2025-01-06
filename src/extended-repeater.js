cd83242f3cd02b995b64dfbccadf1715649eff54
DeFarh <163840209+DeFarh@users.noreply.github.com>
2025-01-06 15:41:46 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+
+  if (!options.repeatTimes) {
+    result += str;
+    if (options.addition) {
+      if (options.additionRepeatTimes) {
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+          result += options.addition;
+        }
+      } else {
+        result += options.addition;
+      }
+    }
+  }
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    if (!options.separator) {
+      
+      if (options.addition) {
+        if (options.additionRepeatTimes) {
+          result += str;
+          for (let i = 0; i < options.additionRepeatTimes; i++) {
+            result += options.addition;
+          }
+        } else {
+          result += str + options.addition;
+        }
+        if (i < options.repeatTimes - 1) {
+          result += '+'
+        }
+      }
+      
+      if (!options.addition) {
+        if (i === options.repeatTimes - 1) {
+            result += str
+        } else {
+            result += str + '+';
+        }
+      }
+    }
+
+    if (options.separator) {
+
+      if (options.addition) {
+        if (options.additionRepeatTimes) {
+            result += str;
+            for (let i = 0; i < options.additionRepeatTimes; i++) {
+              result += options.addition;
+            }
+            } else {
+              result += str + options.addition;
+            }
+            if (i < options.repeatTimes - 1) {
+              result += options.separator;
+            }
+    }
+
+      if (!options.addition) {
+        if (i === options.repeatTimes - 1) {
+            result += str
+        } else {
+            result += str + options.separator;
+        }
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
